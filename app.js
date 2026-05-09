228bbf3d04a3f53bb21bffc8bcd92658c2f74dc3
fatih0281 <124438691+fatih0281@users.noreply.github.com>
2026-05-09 19:26:50 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
-import { getFirestore, doc, setDoc, getDoc, updateDoc, collection, onSnapshot } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-firestore.js";
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-// KENDİ FİREBASE BİLGİLERİNİ BURAYA YAPIŞTIR
-const firebaseConfig = {
-  apiKey: "AIzaSyA2QmLBjaCFQmV6DjhoCiBG0HihY4AALXk",
-  authDomain: "focusflow-85056.firebaseapp.com",
-  projectId: "focusflow-85056",
-  storageBucket: "focusflow-85056.firebasestorage.app",
-  messagingSenderId: "221972136729",
-  appId: "1:221972136729:web:35c3526501b678038afb88",
-};
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyA2QmLBjaCFQmV6DjhoCiBG0HihY4AALXk",
+    authDomain: "focusflow-85056.firebaseapp.com",
+    projectId: "focusflow-85056",
+    storageBucket: "focusflow-85056.firebasestorage.app",
+    messagingSenderId: "221972136729",
+    appId: "1:221972136729:web:35c3526501b678038afb88",
+    measurementId: "G-K4L98YC68V"
+  };
 
-// Firebase'i Başlat
-const firebaseApp = initializeApp(firebaseConfig);
-const db = getFirestore(firebaseApp);
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
 
 document.addEventListener("DOMContentLoaded", () => {
     const loader = document.getElementById('loader');
